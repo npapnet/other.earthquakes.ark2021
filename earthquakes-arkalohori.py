f44c25a18c125b46d6d1f550c95d305260aeb625
N. Papadakis (d-g5-5590) <npapnet@gmail.com>
2021-10-02 23:59:05 +0300

    merged file

--- earthquakes-arkalohori.py
+++ earthquakes-arkalohori.py
@@ -18,8 +18,13 @@ df.mag.plot.hist()
 
 # %%
 max_loc = df.mag.idxmax()
-# %%
+# %%  # additional columns
 df['post'] = df.index < max_loc
+df['date'] = df.timestamp.apply(lambda x: x.date())
+
+dts = [(df.timestamp[i]-df.timestamp[i+1]).total_seconds() for i in range(df.shape[0]-1)]
+dts.append(0)
+df['sec_before'] = dts
 #%%
 fig, axs = plt.subplots(nrows=1, ncols=2, sharey=True)
 
@@ -37,25 +42,24 @@ df.plot(x='timestamp', y='mag', lw=0, marker='.', ax=axs)
 df.iloc[:max_loc+1,:].plot(x='timestamp', y='mag', lw=0, marker='.')
 # %%
 fig, axs = plt.subplots(nrows=1, ncols=1,figsize=(20,15))
-df['date'] = df.timestamp.apply(lambda x: x.date())
 sns.scatterplot(x='timestamp', y='mag', data = df.iloc[:max_loc+1,:], ax =axs)
 sns.lineplot(x='date', y='mag', data = df.iloc[:max_loc+1,:], ax =axs)
 # %%
 plt.show()
-# %%
-
 
 # %%
-# %%
-df.columns
-# %%
-dts = [ (df.timestamp[i] -df.timestamp[i+1]).total_seconds() for i in range(df.shape[0]-1)]
-dts.append(0.0)
-df['dts'] = dts
+
     
 # %%
-df.iloc[:max_loc+1,:].dts.plot.kde(xlim = [0,1e4])
+df.iloc[:max_loc+1,:].sec_before.plot.kde(xlim = [0,1e4])
 # %%
-df.iloc[:max_loc+1,:].dts.plot.hist(xlim = [0,1e4])
+df.iloc[:max_loc+1,:].sec_before.plot.hist(xlim = [0,1e4])
 
+#%%
+df.iloc[:max_loc+1,:].plot.scatter(x='sec_before', y='mag', xlim= (0,10000))
+
+
+# %%
+fig, axs = plt.subplots(nrows=1, ncols=1,figsize=(20,15))
+df.iloc[:max_loc+1,:].plot.scatter(x ='sec_before', y='mag', ax =axs, logx=True,xlim=[1,1e6])
 # %%
